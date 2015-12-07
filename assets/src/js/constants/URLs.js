6e9a5e3549cbc35b6f24e40336756a15217e8b27
JacopoDaeli <jacopo.daeli@gmail.com>
2015-12-07 18:02:44 +0000

    Update URLs

--- assets/src/js/constants/URLs.js
+++ assets/src/js/constants/URLs.js
@@ -1,4 +1,4 @@
 'use strict'
 
-export const BASE_URL = window.BASE_URL || 'http://localhost:3001'
-export const PAGES_URL = `${BASE_URL}/pages`
+export const BASE_URL = window.BASE_URL || 'http://localhost:3000'
+export const PAGES_URL = `${BASE_URL}/pages-api`
